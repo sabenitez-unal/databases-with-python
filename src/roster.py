21190bcefd1419ac6a260ade8125f686454900a4
sabenitez-unal <sabenitez@unal.edu.co>
2026-03-14 15:11:20 -0500

    main ejercicio de roster y db sample

--- src/roster.py
+++ src/roster.py
@@ -33,3 +33,33 @@ CREATE TABLE Member (
 """
     )
 
+def main():
+    make_db()
+
+    file_name = input("Enter the file name: ")
+    if len(file_name) < 1: file_name = "roster_data.json"
+
+    with open(f"files/{file_name}") as file:
+        data = json.load(file)
+
+        for entry in data:
+            name = entry[0]
+            title = entry[1]
+            role = entry[2]
+
+            print((name, title))
+
+            cur.execute("INSERT OR IGNORE INTO User (name) VALUES (?)", (name,))
+            cur.execute("SELECT id FROM User WHERE name = ?", (name,))
+            user_id = cur.fetchone()[0]
+
+            cur.execute("INSERT OR IGNORE INTO Course (title) VALUES (?)", (title,))
+            cur.execute("SELECT id FROM Course WHERE title = ?", (title,))
+            course_id = cur.fetchone()[0]
+
+            cur.execute("INSERT OR IGNORE INTO Member (user_id, course_id, role) VALUES (?, ?, ?)", (user_id, course_id, role))
+            
+            conn.commit()
+
+if __name__ == "__main__":
+    main()
